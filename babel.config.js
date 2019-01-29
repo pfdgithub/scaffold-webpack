e365b8a420ce81748028e245556bf0b4a517096c
liping <liping@wdai.com>
2019-01-29 18:24:20 +0800

    整理babel-plugin

--- babel.config.js
+++ babel.config.js
@@ -28,26 +28,17 @@ module.exports = (api) => {
           "useESModules": true
         }
       ],
-      // Stage 0
-      // "@babel/plugin-proposal-function-bind",
-      // Stage 1
-      // "@babel/plugin-proposal-do-expressions",
-      "@babel/plugin-proposal-export-default-from",
-      // "@babel/plugin-proposal-logical-assignment-operators",
-      // "@babel/plugin-proposal-nullish-coalescing-operator",
-      // "@babel/plugin-proposal-optional-chaining",
-      // "@babel/plugin-proposal-pipeline-operator",
-      // Stage 2
-      // "@babel/plugin-proposal-decorators",
-      "@babel/plugin-proposal-export-namespace-from",
-      // "@babel/plugin-proposal-function-sent",
-      // "@babel/plugin-proposal-numeric-separator",
-      "@babel/plugin-proposal-throw-expressions",
-      // Stage 3
       "@babel/plugin-proposal-class-properties",
-      // "@babel/plugin-proposal-json-strings",
-      "@babel/plugin-syntax-dynamic-import",
-      "@babel/plugin-syntax-import-meta"
+      "@babel/plugin-proposal-decorators",
+      "@babel/plugin-proposal-export-default-from",
+      "@babel/plugin-proposal-export-namespace-from",
+      "@babel/plugin-proposal-nullish-coalescing-operator",
+      "@babel/plugin-proposal-object-rest-spread",
+      "@babel/plugin-proposal-optional-catch-binding",
+      "@babel/plugin-proposal-optional-chaining",
+      "@babel/plugin-proposal-private-methods",
+      "@babel/plugin-proposal-throw-expressions",
+      "@babel/plugin-syntax-dynamic-import"
     ]
   };
 };
