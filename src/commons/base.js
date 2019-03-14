b2cdff2aaf3f85e992f3673bfdfb6791b3cf8844
liping <liping@wdai.com>
2019-03-14 16:58:51 +0800

    不在单独引入 Promise Map Set 以及兼容性较好的 raf

--- src/commons/base.js
+++ src/commons/base.js
@@ -3,14 +3,6 @@ import 'normalize.css/normalize.css';
 import 'styles/base.less';
 
 // 载入公共脚本
-import promise from 'core-js/library/fn/promise'; // webpack natty-fetch
-window.Promise || (window.Promise = promise);
-import map from 'core-js/library/fn/map'; // react 16
-window.Map || (window.Map = map);
-import set from 'core-js/library/fn/set'; // react 16
-window.Set || (window.Set = set);
-import raf from 'raf'; // react 16
-window.requestAnimationFrame || (raf.polyfill());
 
 // // 载入简单 SW 示例
 // import 'sources/sw.simple';
